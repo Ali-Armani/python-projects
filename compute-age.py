2fba729b8a3ea3a84e6b7ea75627e262c6d99a77
Ali Armani <armani.ali@proton.me>
2026-08-23 00:16:34 +0330

    feat: add Python practice exercise

---
+++ compute-age.py
@@ -0,0 +1,9 @@
+'''
+☆☆☆ Computing The Applicant's Age ☆☆☆
+'''
+
+year=input("The Year You Were Born:")
+year_to_int=int(year)
+print("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆")
+print("Your Age:")
+print(2026-year_to_int)
