b2f6ce5278f39c84e38636218f368b70927c4e69
Ali Armani <armani.ali@proton.me>
2026-08-23 00:20:33 +0330

    feat: add Python practice exercise
    try
    except
    finally

---
+++ error-handling.py
@@ -0,0 +1,11 @@
+try:
+	number1=input("Number 1:")
+	number2=input("Number 2:")
+	number1=int(number1)
+	number2=int(number2)
+	print("Sum=")
+	print(number1+number2)
+except:
+	print("Please only enter integers :)")
+finally:
+	print("See you again♡")
