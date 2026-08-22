1d86baef0228eb7cc107a243e0af5092abf94bfd
Ali Armani <armani.ali@proton.me>
2026-08-23 00:27:53 +0330

    feat: add Python practice exercise
    if
    elif
    else

---
+++ if_traffic_light.py
@@ -0,0 +1,10 @@
+green=True
+yellow=False
+red=False
+
+if green==True and yellow==False and red==False:
+	print("Move!")
+elif green==False and yellow==True and red==False:
+  print("Caution!")
+else:
+  print("Stop!")
