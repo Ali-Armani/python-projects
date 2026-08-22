d13cec95c08a8aaf59f8e9a67ae90710c53dd225
Ali Armani <armani.ali@proton.me>
2026-08-23 00:29:05 +0330

    feat: add Python practice exercise
    name
    age
    phone
    email

---
+++ input.py
@@ -0,0 +1,16 @@
+"""
+______---------______---------______--------_____----
+Recieving The Applicant's Personal Info
+______---------______--------______---------______
+"""
+
+name=input("Your Full Name:")
+age=input("Your Age")
+phone=input("Your Phone Number")
+email=input("Your Email:")
+
+print("______------______-------_____")
+print(name)
+print(age)
+print(phone)
+print(email)
