9462c9fc7b3f58e4546adc38245c51e140707f92
Ali Armani <armani.ali@proton.me>
2026-08-25 21:05:39 +0330

    feat: add Python practice exercise

---
+++ Math_Module.py
@@ -0,0 +1,20 @@
+import math
+
+#Power
+print(pow(2,5))
+print(math.pow(2,5))
+
+
+#Pi
+print(math.pi)
+
+
+#Absolute
+print(abs(-12))
+print(abs(12))
+print(abs(0))
+
+
+#minimum maximum
+print(max(294,7,-6.5,73))
+print(min(-12,399,5,0))
