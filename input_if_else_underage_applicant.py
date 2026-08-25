1f3d6f1d3409f5274a8eba320bc09cb58f1d4927
Ali Armani <armani.ali@proton.me>
2026-08-25 21:01:13 +0330

    feat: add Python practice exercise

---
+++ input_if_else_underage_applicant.py
@@ -0,0 +1,7 @@
+number=input("What year were you born in?")
+number_to_int=int(number)
+if number_to_int>=2007:
+	print("Sorry, you are underage!")
+else:
+	email=input("Your Email:")
+	print(email)
